6f9064eaf82426ba743f72f44d24320042ee7905
Martin Cervenka <martincervenka10@gmail.com>
2019-10-09 14:31:24 +0200

    heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "app.css";
+import "./app.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
 
 ReactDOM.render(
   <App />,
